e6324f8c963678df8a23f4510a8506e4f300a19e
Sophie McGarity <sophiemcgarity7@gmail.com>
2020-03-12 20:03:41 +0000

    Refactoring

--- RB101_Programming_Foundations/lesson_2/rock_paper_scissors.rb
+++ RB101_Programming_Foundations/lesson_2/rock_paper_scissors.rb
@@ -1,13 +1,27 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICE = {
+  r:  'rock',
+  p:  'paper',
+  s: 'scissors',
+  l:  'lizard',
+  k: 'spock'
+}
+
+puts VALID_CHOICE
+
+WIN_CONDITIONS = {
+  r: [:s, :l],
+  p: [:r, :k],
+  s: [:p, :l],
+  l: [:p, :k],
+  k: [:r, :s],
+}
 
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  WIN_CONDITIONS[first].include?(second)
 end
 
 def display_results(player, computer)
@@ -20,20 +34,42 @@ def display_results(player, computer)
   end
 end
 
-loop do
+def get_player_choice
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    prompt("Choose one: (r)ock (p)aper (s)cissors (l)izard spoc(k)")
+    choice = gets.chomp.downcase
 
-    if VALID_CHOICES.include?(choice)
+    if VALID_CHOICE.include?(choice)
       break
     else
       prompt("That's not a valid choice.")
     end
+    
   end
+end
 
-  computer_choice = VALID_CHOICES.sample
+def welcome_message
+  prompt('Rock, Paper, Scissors, Lizard, Spock')
+  prompt('Rules: 
+          Scissors cuts paper,
+          paper covers rock, 
+          rock crushes lizard, 
+          lizard poisons Spock, 
+          Spock smashes scissors, 
+          scissors decapitates lizard, 
+          lizard eats paper, 
+          paper disproves Spock, 
+          Spock vaporizes rock, 
+          rock crushes scissors.')
+end
+
+loop do
+  welcome_message
+  
+  get_player_choice
+
+  computer_choice = VALID_CHOICE.sample
 
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
@@ -41,6 +77,7 @@ loop do
 
   prompt("Do you want to play again? ('y' to play again)")
   answer = Kernel.gets().chomp()
+
   break unless answer.downcase().start_with?('y')
 end
 
