d39d31a9bdd4b572b39005f98e198751836d4025
Sophie McGarity <sophiemcgarity7@gmail.com>
2020-06-01 23:08:43 +0000

    second attempt

--- RB101_RB109_Small_Problems/easy_7.rb
+++ RB101_RB109_Small_Problems/easy_7.rb
@@ -1,271 +1,230 @@
-require 'pry'
-# 1 ----------------------------------------------------------------------
+# 1 --------------------------------------------------------------------------------------------------------------
+# Combine Two Lists
 
-# def interleave(array1, array2)
-#   new_array = []
-
-#   array1.each_with_index do |element, index|
-#     new_array << element << array2[index]
+# def interleave(arr1, arr2)
+#   new_arr = []
+#   index = 0
+#   arr1.size.times do 
+#     new_arr << arr1[index] << arr2[index]
+#     index += 1
 #   end
-
-#   new_array
+#   new_arr
 # end
 
-# p interleave([1, 2, 3], ['a', 'b', 'c'])
+# def interleave(arr1, arr2)
+#   arr1.zip(arr2).flatten
+# end
 
-# 2 ----------------------------------------------------------------------
+# p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+
+
+# 2 -------------------------------------------------------------------------------------------------------------
 # Lettercase Counter
 
 # def letter_case_count(string)
-#   result = { lowercase: 0, uppercase: 0, neither: 0 }
-#   chars = string.chars
-  
-#   chars.each do |char|
-#     case char
-#     when /[a-z]/
-#       result[:lowercase] += 1
-#     when /[A-Z]/
-#       result[:uppercase] += 1
-#     else
-#       result[:neither] += 1
-#     end  
+#   counters = { lowercase: 0, uppercase: 0, neither: 0 }
+#   string.chars.each do |char|
+#     case
+#     when ('a'..'z').include?(char) then counters[:lowercase] += 1
+#     when ('A'..'Z').include?(char) then counters[:uppercase] += 1
+#     else counters[:neither] += 1
+#     end
 #   end
-  
-#   result
+#   counters
 # end
-  
-# letter_case_count('abCdef 123')
-# letter_case_count('AbCd +Ef')
 
-# 3 ----------------------------------------------------------------------
+# p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+# p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+# p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+# p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+
+# 3 ----------------------------------------------------------------------------------------------------
 # Capitalize Words
 
-# def word_cap(input_string)
-#   words = input_string.split(' ')
-#   caps = []
-  
-#   words.each { |word| caps << word.capitalize }
-#   result = caps.join(" ")
-#   result
+# def word_cap(string)
+#   capitalized_words = []
+#   string.split(' ').each { |word| capitalized_words << word.capitalize }
+#   capitalized_words.join(' ')
 # end
 
-# def word_cap(words)
-#   words_array = words.split.map do |word|
-#     word.capitalize
+# def word_cap(string)
+#   capitalized_words = []
+  
+#   string.upcase.split(' ').each do |word| 
+#     capitalized_words << word[0] + word[1..-1].downcase
 #   end
-#   words_array.join(' ')
-# end
-
-# without capitalize method
-
-# def word_cap(input_string)
-#   words = input_string.split(' ')
-#   caps = []
   
-#   words.each { |word| caps << word[0].upcase + word[1..word.length] }
-  
-#   result = caps.join(" ")
-#   result
+#   capitalized_words.join(' ')
 # end
 
-# def word_cap(words)
-#   words_array = words.split(' ')
-#   words_array.each do |word|
-#     word[0] = word[0].upcase
-#     word[1..-1] = word[1..-1].downcase
-#   end
-#   words_array.join(' ')
-# end
+# p word_cap('four score and seven') == 'Four Score And Seven'
+# p word_cap('the javaScript language') == 'The Javascript Language'
+# p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
 
-# puts word_cap('four score and seven')
-# puts word_cap('the javaScript language')
-# puts word_cap('this is a "quoted" word')
-
-# 4 ----------------------------------------------------------------------
+# 4 ---------------------------------------------------------------------------------------------------------------------
 # Swap case
 
-# def swapcase(words)
-#   swapped_chars = words.split('').map do |char|
-#     case char
-#     when char.downcase
-#       char.upcase
-#     when char.upcase
-#       char.downcase
+# def swapcase(string)
+#   swapped_chars = []
+#   string.chars.each do |char|
+#     if char == char.downcase
+#       swapped_chars << char.upcase
 #     else
-#       char
+#       swapped_chars << char.downcase
 #     end
 #   end
-#   swapped_chars.join('')
+#   swapped_chars.join
 # end
 
-# puts swapcase('CamelCase')
-# puts swapcase('Tonight on XYZ-TV')    
 
-# 5 ----------------------------------------------------------------------
+# p swapcase('CamelCase') == 'cAMELcASE'
+# p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
+
+
+# 5 --------------------------------------------------------------------------------------------------------------
 # Staggered Caps (part 1)
+
 # def staggered_case(string)
-#   result = ''
-#   need_upper = true
-  
-#   string.chars.each do |char|
-#     if need_upper == true
-#       result += char.upcase
-#       need_upper = false
-#     elsif need_upper == false
-#       result += char.downcase
-#       need_upper = true
-#     else
-#       result += char
+#   staggered_chars = []
+#   string.chars.each_with_index do |char, index|
+#     if index.even?
+#       staggered_chars << char.upcase
+#     elsif index.odd?
+#       staggered_chars << char.downcase
 #     end
 #   end
-#   result
+#   staggered_chars.join
 # end
 
-# def staggered_case(string)
-#   result = ''
-#   need_upper = true
-#   string.chars.each do |char|
-#     if need_upper
-#       result += char.upcase
-#     else
-#       result += char.downcase
-#     end
-#     need_upper = !need_upper
-#   end
-#   result
-# end
+# p staggered_case('I Love Launch School!') #== 'I LoVe lAuNcH ScHoOl!'
+# p staggered_case('ALL_CAPS') #== 'AlL_CaPs'
+# p staggered_case('ignore 77 the 444 numbers') #== 'IgNoRe 77 ThE 444 NuMbErS'
 
-# puts staggered_case('I Love Launch School!')
-# puts staggered_case('ALL_CAPS')
-# puts staggered_case('ignore 77 the 444 numbers')
-
-# 6 ----------------------------------------------------------------------
-# Staggered Caps (part 2)
-# def staggered_case(string)
-#   result = ''
-#   need_upper = true
-  
-#   string.chars.each do |char|
-#     if char == " "
-#       result += char
-#       next
-#     elsif need_upper == true
-#       result += char.upcase
-#       need_upper = false
-#     elsif need_upper == false
-#       result += char.downcase
-#       need_upper = true
-#     end
-#   end
-  
-#   result
-# end
+# 6 ----------------------------------------------------------------------------------------------------------------
 
 # def staggered_case(string)
-#   result = ''
-#   need_upper = true
-  
+#   staggered_chars = []
+#   switch = true
+
 #   string.chars.each do |char|
-#     if char =~ /[a-z]/i
-#       if need_upper
-#         result += char.upcase
+#     if ('a'..'z').include?(char.downcase)
+#       if switch == true
+#         staggered_chars << char.upcase
 #       else
-#         result += char.downcase
+#         staggered_chars << char.downcase
 #       end
-#       need_upper = !need_upper
+#       switch = !switch
 #     else
-#       result += char
+#       staggered_chars << char
 #     end
 #   end
-#   result
+
+#   staggered_chars.join
 # end
 
-# puts staggered_case('I Love Launch School!') 
-# puts staggered_case('ALL CAPS')
-# puts staggered_case('ignore 77 the 444 numbers')
+# def staggered_case(string, include_numbers)
+#   staggered_chars = []
+#   switch = true
+  
+#   if include_numbers == true
+#     string.chars.each do |char|
+#       if switch == true
+#         staggered_chars << char.upcase
+#       else
+#         staggered_chars << char.downcase
+#       end
+#       switch = !switch
+#     end
+#   else
+#     string.chars.each do |char|
+#       if ('a'..'z').include?(char.downcase)
+#         if switch == true
+#           staggered_chars << char.upcase
+#         else
+#           staggered_chars << char.downcase
+#         end
+#         switch = !switch
+#       else
+#         staggered_chars << char
+#       end
+#     end
+#   end
 
+#   staggered_chars.join
+# end
 
-# 7 -----------------------------------------------------------
+# p staggered_case('I Love Launch School!', true) 
+# p staggered_case('ALL CAPS', false)
+# p staggered_case('ignore 77 the 444 numbers', true)
+
+# 7 -----------------------------------------------------------------------------------------------------
 # Multiplicative Average
 
 # def show_multiplicative_average(array)
-#   multiplied_result = 1.to_f
-  
-#   array.each { |num| multiplied_result *= num }
-#   # multiplied_result = array.reduce(:*)
-  
-#   average = (multiplied_result / array.length)
-  
+#   total = array.reduce(:*).to_f
+#   size = array.size
+#   average = total / size
 #   puts "The result is #{format('%.3f', average)}"
 # end
 
-# show_multiplicative_average([3, 5])
-# show_multiplicative_average([6])
 # show_multiplicative_average([2, 5, 7, 11, 13, 17])
 
-
-# 8 -----------------------------------------------------------
-# Multiply Lists
+# 8 -------------------------------------------------------------------------------------------------------
+# Multiple Lists
 
 # def multiply_list(array1, array2)
-#   # output = []
+#   result = []
   
-#   # array1.each_with_index do |num, index|
-#   #   output << num * array2[index]
-#   # end
+#   array1.each_with_index do |num, index|
+#     result << num * array2[index]
+#   end
   
-#   # output
+#   result
 # end
 
 # def multiply_list(array1, array2)
 #   array1.zip(array2).map { |arr| arr.reduce(:*) }
 # end
 
-# p multiply_list([3, 5, 7], [9, 10, 11]) # [27, 50, 77]
+# p multiply_list([3, 5, 7], [9, 10, 11])
 
-# 9 -----------------------------------------------------------
-# Multiply All Pairs
+# 9 ------------------------------------------------------------------------------------------------------
+# Multiple All Pairs
 
-# def multiply_all_pairs(array1, array2)
-#   result = []
-  
-#   array1.each do |num|
-#     array2.each do |el|
-#       result << num * el
+# def multiply_all_pairs(arr1, arr2)
+#   pairs = []
+#   arr1.each do |num1|
+#     arr2.each do |num2|
+#       pairs << [num1, num2]
 #     end
 #   end
   
-#   result.sort
+#   pairs.map { |arr| arr.reduce(:*) }.sort
 # end
 
-# p multiply_all_pairs([2, 4], [4, 3, 1, 2]) #== [2, 4, 4, 6, 8, 8, 12, 16]
+# p multiply_all_pairs([2, 4], [4, 3, 1, 2])
 
-
-# 10 --------------------------------------------------------------
+# 10 -------------------------------------------------------------------------------------------------------
 # The End Is Near But Not Here
 
 # def penultimate(string)
-#   words = string.split(' ')
-#   words[-2]
+#   words_array = string.split(' ')
+#   words_array[-2]
 # end
 
 # p penultimate('last word') == 'last'
 # p penultimate('Launch School is great!') == 'is'
 
-# Further exploration
-# middle number
-  
-def middle_word(string)
-  words = string.split(' ')
-  index = (words.length.to_f / 2.to_f).round(half: :up)
-  if index < 1
-    string
-  else
-    words[index - 1]
-  end
-end
+# find the middle word from a sentence
 
-p middle_word('School is great!') # => 'is'
-p middle_word('Launch School is great!') # => 'School'
-p middle_word('') # => ''
-p middle_word('School') # => 'School'
+# def middle_word(string)
+#   words = string.split
+#   index = words.length / 2
+#   case
+#     when words.length <= 1 then string
+#     when words.length.even? then words[index - 1]
+#     when words.length.odd? then words[index]
+#   end
+# end
+
+# p middle_word('one two three')
