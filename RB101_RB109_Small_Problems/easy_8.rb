58291a7ba74c72cd9259d3d20636e57a813d61b5
Sophie McGarity <sophiemcgarity7@gmail.com>
2020-06-02 21:24:20 +0000

    second attempt

--- RB101_RB109_Small_Problems/easy_8.rb
+++ RB101_RB109_Small_Problems/easy_8.rb
@@ -1,146 +1,116 @@
-require 'pry'
-# 1. -----------------------------------------------
+# 1 ------------------------------------------------------------------------------------------------------------------
 # Sum of Sums
 
 # def sum_of_sums(array)
-#   current_value = 0
-#   result = []
-  
-#   array.each { |num| result << current_value += num }
-  
-#   result.reduce(:+)
+#   total = 0
+#   total_values = []
+#   array.each do |num|
+#     total_values << total += num
+#   end
+#   total_values.reduce(:+)
 # end
 
-# p sum_of_sums([3, 5, 2]) # (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
-# p sum_of_sums([1, 5, 7, 3]) #== (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
-# p sum_of_sums([4]) #== 4
-# p sum_of_sums([1, 2, 3, 4, 5]) #== 35
+# p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
 
-# 2 -----------------------------------------------------------------
+
+# 2 -------------------------------------------------------------------------------------------------------------
 # Madlibs
 
-# def prompt(string)
-#   puts ">> #{string}"
-# end
-
-# prompt('Enter a noun: ')
+# puts "Enter a noun: "
 # noun = gets.chomp
-
-# prompt('Enter a verb: ')
+# puts "Enter a verb: "
 # verb = gets.chomp
-
-# prompt('Enter a adjective: ')
+# puts "Enter an adjective: "
 # adjective = gets.chomp
-
-# prompt('Enter a adverb: ')
+# puts "Enter an adverb"
 # adverb = gets.chomp
 
-# sentence_one = "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
+# puts "Do you #{verb} your #{adjective} #{noun} #{adverb}?"
 
-# sentence_two = "You like to #{verb} with your #{adjective} #{noun} #{adverb}?"
-
-# puts [sentence_one, sentence_two].sample
-
-# 3. ------------------------------------------------------------
-# Leading substrings
+# 3 ---------------------------------------------------------------------------------------------------------
+# Leading Substrings
 
 # def substrings_at_start(string)
-#   result = []
-#   previous_char = ''
-#   chars = string.split('')
-  
-#   chars.map do |char|
-#     result << previous_char + char
-#     previous_char = result[-1]
+#   substrings = []
+#   string.size.times do |index|
+#     substrings << string[0..index]
 #   end
-  
-#   result
-# end
+#   substrings
+# end 
 
-# 4. -----------------------------------------------------
+# p substrings_at_start('abc') == ['a', 'ab', 'abc']
+# p substrings_at_start('a') == ['a']
+# p substrings_at_start('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
+
+# 4 ------------------------------------------------------------------------------------------------------
 # All Substrings
 
 # def substrings(string)
-#   string.gsub!(/[^A-Za-z0-9_]/, '')
-#   result = []
-  
-#   (0..string.size).each do |start_index|
-#     this_substring = string[start_index..-1]
-#     result.concat(substrings_at_start(this_substring))
+#   all_substrings = []
+#   string.size.times do |start_index|
+#     all_substrings.concat(substrings_at_start(string[start_index..-1]))
 #   end
-  
-#   result
+#   all_substrings
 # end
 
-# 5 -----------------------------------------------------------------
-# Palindrom Substrings
+# p substrings('abcde')
+
+# 5 --------------------------------------------------------------------------------------------
+# Palidromic Substrings
 
 # def palindromes(string)
-  
-#   result = []
-#   substrings(string).each do |substring|
-#     result << substring if palindrome?(substring)
+#   string.gsub!(/[^a-zA-Z]/, '')
+#   palins = []
+#   substrings(string).each do |str|
+#     palins << str if str == str.reverse && str.size > 1
 #   end
-  
-#   result
+#   palins
 # end
 
-# def palindrome?(string)
-#   string.downcase == string.downcase.reverse && string.size > 1 
-# end
+# p palindromes('mada-m-')
 
-# p palindromes('abcd')
-# p palindromes('madam')
-# p palindromes('hello-madam-did-madam-goodbye') 
+# 6 ----------------------------------------------------------------------------------------
+# Fizzbuzz
 
-# 6 ------------------------------------------------------------------
-# fizzbuzz
-
-# def fizzbuzz(start_num, end_num)
-#   output = []
-  
-#   (start_num..end_num).each do |num|
-#     if num % 3 == 0 && num % 5 == 0
-#       output << 'FizzBuzz'
-#     elsif num % 3 == 0
-#       output << 'Fizz'
-#     elsif num % 5 == 0
-#       output << 'Buzz'
-#     else
-#       output << num
+# def fizzbuzz(num1, num2)
+#   result = []
+#   (num1..num2).each do |num|
+#     case
+#     when num % 5 == 0 && num % 3 == 0 then result << 'FizzBuzz'
+#     when num % 5 == 0                 then result << 'Buzz'
+#     when num % 3 == 0                 then result << 'Fizz'
+#     else result << num.to_s
 #     end
 #   end
-  
-#   output.join(', ')
+#   result.join(', ')
 # end
 
 # p fizzbuzz(1, 15)
 
-# 7 -------------------------------------------------------------------------------
-# Double char (part 1)
+# 7 ---------------------------------------------------------------------------------------
+# Double Char (part 1)
 
-def repeater(string)
-  result = ''
-  chars = string.split('')
-  
-  chars.map { |char| result << char << char }
-  
-  result
-end
+# def repeater(string)
+#   result = ''
+#   string.chars.each { |char| result << char << char }
+#   result
+# end
 
-# p repeater("Good job!") #== "GGoooodd  jjoobb!!"
-# p repeater('') #== ''
-# p repeater('Hello') #== "HHeelllloo"
+# p repeater('Hello') == "HHeelllloo"
 
-# 8 ---------------------------------------------------------------------------
-# CONSONANTS = %w(b c d f g h j k l m n p q r s t v w x y z)
-  
+# 8 --------------------------------------------------------------------------------------
+# Double char (part 2)
+
+# CONSONANTS = []
+
+# ('a'..'z').map do |letter|
+#   ('aeiou').include?(letter) ? next : CONSONANTS << letter
+# end
+
 # def double_consonants(string)
 #   result = ''
-  
-#   string.each_char do |char|
-#     result << char
-#     result << char if CONSONANTS.include?(char.downcase)
+#   string.chars.each do |char|
+#     CONSONANTS.include?(char.downcase) ? result << (char * 2) : result << char
 #   end
   
 #   result
@@ -151,34 +121,23 @@ end
 # p double_consonants("July 4th") == "JJullyy 4tthh"
 # p double_consonants('') == ""
 
-# 9 -------------------------------------------------------------------------------
+# 9 ---------------------------------------------------------------------------------------------
 # Convert number to reversed array of digits
 
 # def reversed_number(num)
-#   string = num.to_s
-#   string.reverse.to_i
+#   num.to_s.reverse.to_i
 # end
 
-# p reversed_number(12345) == 54321
-# p reversed_number(12213) == 31221
-# p reversed_number(456) == 654
-# p reversed_number(12000) == 21 # No leading zeros in return value!
-# p reversed_number(12003) == 30021
-# p reversed_number(1) == 1
+# p reversed_number(12345)
 
-
-# 10 ----------------------------------------------------------------------------------
+# 10 --------------------------------------------------------------------------------------
 # Get the middle character
 
 # def center_of(string)
-#   index = string.length / 2
-  
-#   if string.length.odd?
-#     string[index] 
-#   else 
-#     string[index - 1] + string[index]
-#   end
+#   index = (string.size / 2)
+#   string.size.odd? ? string[index] : string[index - 1, 2]
 # end
-
-# p center_of('I love ruby')
-# p center_of('Launch') #== 'un'
+  
+  
+# p center_of('I love ruby') == 'e'
+# p center_of('Launch') == 'un'
