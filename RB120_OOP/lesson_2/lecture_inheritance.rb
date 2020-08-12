5eb4925d92190bcf758b7d7d8d551a274d078c3b
Sophie McGarity <sophiemcgarity7@gmail.com>
2020-08-12 00:15:23 +0000

    redo questions and explanation

--- RB120_OOP/lesson_2/lecture_inheritance.rb
+++ RB120_OOP/lesson_2/lecture_inheritance.rb
@@ -1,9 +1,9 @@
-# 1 //////////////////////////////////////////////////////////////////////
+# 1 ----------------------------------
 # class Dog
 #   def speak
 #     'bark!'
 #   end
-  
+
 #   def swim
 #     'swimming!'
 #   end
@@ -16,19 +16,22 @@
 # end
 
 # teddy = Dog.new
-# puts teddy.speak
-# puts teddy.swim
+# puts teddy.speak           # => "bark!"
+# puts teddy.swim           # => "swimming!"
 
-# george = Bulldog.new
-# puts george.speak
-# puts george.swim
+# karl = Bulldog.new
+# puts karl.speak           # => "bark!"
+# puts karl.swim            # => "can't swim!"
 
-# 2 //////////////////////////////////////////////////////////////////////////
+# On lines 12 - 16 the Bulldog class is defined as a subclass of Dog. The bulldog class inherits the methods from the super class so on line 23 the bulldog object has access to the speak method.
+# On lines 13 - 15 the swim method inherited from the Dog super class is overidden
+
+# 2 -----------------------------------
 # class Animal
 #   def run
 #     'running!'
 #   end
-  
+
 #   def jump
 #     'jumping!'
 #   end
@@ -39,12 +42,12 @@
 #     'bark!'
 #   end
   
-#   def fetch
-#     'fetching!'
-#   end
-  
 #   def swim
 #     'swimming!'
+#   end
+
+#   def fetch
+#     'fetching!'
 #   end
 # end
 
@@ -54,18 +57,27 @@
 #   end
 # end
 
-# kitty = Cat.new
-# puts kitty.jump
+# On lines 31 - 39 the Animal class is defined and contains two instance methods run and jump
+# On lines 41 - 53 the Dog class is defined as a subclass of Animal. The dog class contains the instance methods speak, swim and fetch
+# On lines 55 - 59 the Cat class is defined as a subclass of Animal and contains one instance method, speak
 
-# 3 ///////////////////////////////////////////////////////////////////////////
+# 3 ------------------------
+          # Animal
+            # run
+            # jump
+  # Dog                # Cat
+    # speak              # speak
+    # swim
+    # fetch
+    
+  # Bulldog
+    # swim
+
+# 4 ------------------------
+# Dog   # Cat
 # Animal
-  # run
-  # jump
-# Dog      # Cat
-  # speak    # speak
-  # fetch
-  # swim 
-  
-# 4 ////////////////////////////////////////////////////////////////////////////
-# What is the method lookup path and how is it important?
-# The order in which called methods are searched for, if found in the first class it will not continue to search for the method in class higher up the heirarchy
+# Object
+# Kernel
+# BasicObject
+
+# The method lookup path is the order in which Ruby looks through classes to find a method that has been called
