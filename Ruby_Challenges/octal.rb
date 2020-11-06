f6e77fca9d024c20438c76c278bcbb26c9a07261
Sophie McGarity <sophiemcgarity7@gmail.com>
2020-11-06 20:24:34 +0000

    third draft added octal validation

--- Ruby_Challenges/octal.rb
+++ Ruby_Challenges/octal.rb
@@ -1,17 +1,15 @@
 # 3 - Octal
 class Octal
   def initialize(num)
-    @nums = num.split('').map(&:to_i)
+    @nums = num.split('').map(&:to_i) unless num.match?(/[a-zA-Z89]/)
   end
 
   def to_decimal
+    return 0 if @nums == nil
     digits = []
     @nums.map.with_index do |item, index|
       digits << item * 8 ** (@nums.size - index - 1)
     end
     digits.reduce(:+)
   end
-
 end
-
-p Octal.new('233').to_decimal  # 155
