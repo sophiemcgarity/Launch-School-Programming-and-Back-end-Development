32a6a593f1d5eb411f217d0cbd995a594e88508e
Sophie McGarity <sophiemcgarity7@gmail.com>
2020-12-02 23:15:34 +0000

    third refactor

--- Ruby_Challenges/sum_of_multiples.rb
+++ Ruby_Challenges/sum_of_multiples.rb
@@ -4,22 +4,15 @@ class SumOfMultiples
     @multiple_input = multiple_input
   end
 
+  def self.to(n)
+    new(3, 5).to(n)
+  end
+
   def to(n)
     result_multiples = []
-
     (0...n).each do |num|
-      @multiple_input.each do |multiple|
-        result_multiples << num if num % multiple == 0
-      end
+      result_multiples << num if @multiple_input.any? { |multiple| num % multiple == 0 }
     end
-
-    result_multiples.uniq.reduce(:+)
-  end
-
-  def self.to(n)
-    (0...n).select { |number| number % 3 == 0 || number % 5 == 0 }.reduce(:+)
+    result_multiples.reduce(:+)
   end
 end
-
-sum = SumOfMultiples.new(4, 6).to(15)
-p sum
